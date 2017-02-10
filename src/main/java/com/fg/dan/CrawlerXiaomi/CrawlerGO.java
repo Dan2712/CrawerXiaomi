a8dbabee5fdf7b0490f0cda1f3d9672089abaf9a
dan <dan.ge@dji.com>
2017-02-10 20:39:52 +0800

    cookie almost done

--- src/main/java/com/fg/dan/CrawlerXiaomi/CrawlerGO.java
+++ src/main/java/com/fg/dan/CrawlerXiaomi/CrawlerGO.java
@@ -1,8 +1,16 @@
 package com.fg.dan.CrawlerXiaomi;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
@@ -20,38 +28,88 @@ public class CrawlerGO
     {
     	System.getProperties().setProperty("webdriver.chrome.driver", "E:" + File.separator + "psnl" + File.separator + "chromedriver.exe");
         WebDriver webDriver = new ChromeDriver();
+        loadCookie(webDriver);
         webDriver.get("https://wx.xiaomiquan.com/dweb/#/login");
         webDriver.switchTo().defaultContent();
-        try {	    //不停的检测，一旦当前页面URL不是登录页面URL，就说明浏览器已经进行了跳转
+        try {	   
             while (true) {
                 Thread.sleep(500L);
                 if (!webDriver.getCurrentUrl().startsWith("https://wx.xiaomiquan.com/dweb/#/login")) {
                     break;
                 }
             }
+            
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }	//获取cookie，上面一跳出循环我认为就登录成功了，当然上面的判断不太严格，可以再进行修改
-        Set<Cookie> cookies = webDriver.manage().getCookies();
-        String cookieStr = "";
-        for (Cookie cookie : cookies) {
-            cookieStr += cookie.getName() + "=" + cookie.getValue() + "; ";
         }
-        System.out.println(cookieStr);
-        try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        System.out.println(webDriver.getCurrentUrl());
         
+//        storeCookie(webDriver);
         List<WebElement> elements = webDriver.findElements(By.className("topic-pp"));
         for(int i=0; i<elements.size(); i++) {
-//        	WebElement element = elements.get(i).findElement(By.tagName("p"));
         	System.out.println(elements.get(i).getText());
         }
 //        System.out.println(webElement.getAttribute("outerHTML"));
         webDriver.close();
+    }
+    
+    private static Set<Cookie> storeCookie(WebDriver webDriver) {
+    	Set<Cookie> cookies = webDriver.manage().getCookies();
+    	File cookieFile = new File("Cookie.data");
+    	try {
+        	cookieFile.delete();
+			cookieFile.createNewFile();
+			FileWriter fileWriter = new FileWriter(cookieFile);
+			BufferedWriter writer = new BufferedWriter(fileWriter);
+			
+			for (Cookie cookie : cookies) {
+				writer.write(cookie.getName() + ";" + cookie.getValue() + ";" + cookie.getDomain() + ";" 
+							+ cookie.getPath() + ";" + cookie.getExpiry() + ";" + cookie.isSecure() + "\n");
+			}
+			writer.flush();
+			writer.close();
+			fileWriter.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    
+        return cookies;
+    }
+    
+    private static WebDriver loadCookie(WebDriver webDriver) {
+    	File file = new File("Cookie.data");							
+        FileReader fileReader = null;
+		try {
+			fileReader = new FileReader(file);
+			BufferedReader buffReader = new BufferedReader(fileReader);
+			
+			String strLine = "";
+			while((strLine = buffReader.readLine()) != null) {
+				StringTokenizer tokens = new StringTokenizer(strLine, ";");
+				
+				while(tokens.hasMoreTokens()) {
+					String name = tokens.nextToken();
+					String value = tokens.nextToken();
+					String domain = tokens.nextToken();
+					String path = tokens.nextToken();
+					
+					Date expiry = null;
+					String val;			
+			        if(!(val=tokens.nextToken()).equals("null"))	
+			        	expiry = new Date(val);					
+			        Boolean isSecure = new Boolean(tokens.nextToken());
+			        
+			        Cookie ck = new Cookie(name, value, domain, path, expiry, isSecure);
+			        webDriver.manage().addCookie(ck);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}							
+		
+        return webDriver;
     }
 }
